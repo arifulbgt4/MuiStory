57afacc4ed7713153b6075018d1fdc883105b589
Ariful islam <arifulbgt4@gmail.com>
2023-06-03 22:12:16 +0600

    feat: loading buttons

--- src/stories/IconButton.stories.tsx
+++ src/stories/IconButton.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import IconButton from "@mui/material/IconButton";
-import { Delete, ArrowBack } from "@mui/icons-material";
+import { Delete } from "@mui/icons-material";
+
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof IconButton> = {
   title: "INPUTS/Icon Buttons",
