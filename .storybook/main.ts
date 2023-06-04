995106da161ee2ab28db4cd151814641eff7c6ef
Ariful islam <arifulbgt4@gmail.com>
2023-06-04 12:32:01 +0600

    chor: re-config storybook docs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,11 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../docs/*.mdx",
+    "../docs/**/*.mdx",
+    "../docs/*.stories.@(js|jsx|ts|tsx)",
+    "../docs/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
