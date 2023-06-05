77f78b0a03cc537a1b00c33d72e7674d54e19499
Ariful islam <arifulbgt4@gmail.com>
2023-06-05 13:59:21 +0600

    chore: update storybook features

---
+++ .storybook/manager.ts
@@ -0,0 +1,15 @@
+import { addons } from "@storybook/manager-api";
+import { themes } from "@storybook/theming";
+import { create } from "@storybook/theming/create";
+
+const theme = create({
+  base: "dark",
+  brandTitle: "Mui & Next.js Design System",
+  // brandUrl: 'https://example.com',
+  //   brandImage: "https://storybook.js.org/images/placeholders/350x150.png",
+  brandTarget: "_self",
+});
+
+addons.setConfig({
+  theme,
+});
