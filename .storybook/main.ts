bce96f2896334ad6d400d7f3f56952deed286844
Ariful islam <arifulbgt4@gmail.com>
2023-06-08 22:36:30 +0600

    chor: update storybook latest version

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,30 +1,20 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
-  stories: [
-    "../docs/*.mdx",
-    "../docs/**/*.mdx",
-    "../docs/*.stories.@(js|jsx|ts|tsx)",
-    "../docs/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-styling",
-  ],
+  stories: ["../docs/*.mdx", "../docs/**/*.mdx", "../docs/*.stories.@(js|jsx|ts|tsx)", "../docs/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-styling", "@storybook/addon-mdx-gfm"],
   framework: {
     name: "@storybook/nextjs",
-    options: {},
+    options: {}
   },
   docs: {
     autodocs: "tag",
-    defaultName: "Documentation",
+    defaultName: "Documentation"
   },
   refs: {
     "design-system": {
       title: "Mui default Design System",
-      url: "https://dev--647c84907213dc4172ffdcde.chromatic.com/",
-    },
-  },
+      url: "https://dev--647c84907213dc4172ffdcde.chromatic.com/"
+    }
+  }
 };
 export default config;
