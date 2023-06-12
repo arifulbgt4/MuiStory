a641bed998a78036daa96e366e60e8480b62ab11
Ariful islam <arifulbgt4@gmail.com>
2023-06-12 13:20:18 +0600

    feat: Backdrop sb docs

---
+++ src/theme/overrides/Backdrop.ts
@@ -0,0 +1,9 @@
+import { Theme, Components } from "@mui/material/styles";
+
+const MuiBackdrop: Components<Theme>["MuiBackdrop"] = {
+  styleOverrides: {
+    root: ({ theme, ownerState }) => ({}),
+  },
+};
+
+export default { MuiBackdrop };
--- src/theme/overrides/index.ts
+++ src/theme/overrides/index.ts
@@ -4,11 +4,13 @@ import Button from "./Button";
 import TextField from "./TextField";
 import FormControl from "./FormControl";
 import Alert from "./Alert";
+import Backdrop from "./Backdrop";
 
 export default Object.assign(
   Button,
   Card,
   TextField,
   FormControl,
-  Alert
+  Alert,
+  Backdrop
 ) as Components<any>;
