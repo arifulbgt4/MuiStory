20769b1424388c67f6a0de9ddfcfe589e63b69b8
Ariful islam <arifulbgt4@gmail.com>
2023-06-19 21:35:16 +0600

    feat: AppBar sb docs

---
+++ docs/AppBar.stories.tsx
@@ -0,0 +1,91 @@
+import type { Meta, StoryObj, StoryContext } from "@storybook/react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { argChildren, argProps } from "./utils/formatArgs";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta<typeof AppBar> = {
+  title: "SURFACES/AppBar",
+  component: AppBar,
+  parameters: {
+    docs: {
+      source: { language: "tsx", format: true, type: "dynamic" },
+      description: {
+        component:
+          "The value must be chosen from a predefined set of allowed values.",
+      },
+      canvas: { sourceState: "shown" },
+    },
+    // layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    enableColorOnDark: {
+      control: { type: "boolean" },
+      description: "If `true`, the `color` prop is applied in dark mode.",
+      defaultValue: false,
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof AppBar>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const AppBars: Story = {
+  args: {
+    children: (
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          News
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    ),
+  },
+  parameters: {
+    docs: {
+      source: {
+        transform: (code: string, storyContext: StoryContext): string => `
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
+<AppBar ${argProps(storyContext)}>
+  <Toolbar>
+    <IconButton
+      size="large"
+      edge="start"
+      color="inherit"
+      aria-label="menu"
+      sx={{ mr: 2 }}
+    >
+      <MenuIcon />
+    </IconButton>
+    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+      News
+    </Typography>
+    <Button color="inherit">Login</Button>
+  </Toolbar>
+</AppBar>
+        `,
+      },
+    },
+  },
+};
