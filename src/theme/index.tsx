534b3552ab3766e97a55c8a7ce503dc6f3752cfd
Ariful islam <arifulbgt4@gmail.com>
2023-06-21 03:23:57 +0600

    feat: theming locale langs

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -5,6 +5,7 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { PaletteMode } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import { useMemo, useState, createContext } from "react";
+import * as locales from "@mui/material/locale";
 
 import GlobalStyles from "./utils/GlobalStyles";
 import createEmotionCache from "./utils/createEmotionCache";
@@ -13,15 +14,20 @@ import typography from "./typography";
 import shadowsTheme from "./shadows";
 import componentsOverride from "./overrides";
 
+type SupportedLocales = keyof typeof locales;
+
 const DEFAULT_PALETTE_MODE: PaletteMode = "light";
+const DEFAULT_LOCAL: SupportedLocales = "enUS";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
 export const ColorModeContext = createContext<{
   toggleColorMode: () => void;
+  toggleLocalLang: (mode: SupportedLocales) => void
 }>({
   toggleColorMode: (): void => {},
+  toggleLocalLang: (mode: SupportedLocales) => void
 });
 
 export interface ThemeContextProviderProps {
@@ -34,33 +40,41 @@ const ThemeContextProvider = ({
   emotionCache = clientSideEmotionCache,
 }: ThemeContextProviderProps) => {
   const [mode, setMode] = useState<PaletteMode>(DEFAULT_PALETTE_MODE);
+  const [locale, setLocale] = useState<SupportedLocales>(DEFAULT_LOCAL);
 
-  const colorMode = useMemo(
+  const themingUpdate = useMemo(
     () => ({
       toggleColorMode: (): void => {
         setMode((prevMode: PaletteMode) =>
           prevMode === "light" ? "dark" : "light"
         );
       },
+      toggleLocalLang: (mode: SupportedLocales): void => {
+        setLocale(mode);
+      },
     }),
     []
   );
 
+
   const theme = useMemo(
     () =>
-      createTheme({
-        palette: palette(mode),
-        // spacing: (factor: number) => `${0.5 * factor}rem`,
-        typography,
-        shadows: shadowsTheme(palette(mode) as PaletteOptions),
-        components: componentsOverride,
-      }),
-    [mode]
+      createTheme(
+        {
+          palette: palette(mode),
+          // spacing: (factor: number) => `${0.5 * factor}rem`,
+          typography,
+          shadows: shadowsTheme(palette(mode) as PaletteOptions),
+          components: componentsOverride,
+        },
+        locales[locale]
+      ),
+    [mode, locale]
   );
 
   return (
     <CacheProvider value={emotionCache}>
-      <ColorModeContext.Provider value={colorMode}>
+      <ColorModeContext.Provider value={themingUpdate}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <GlobalStyles />
