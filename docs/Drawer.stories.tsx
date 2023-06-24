015de2cee5ae26213ced617e715b906773d4a440
Ariful islam <arifulbgt4@gmail.com>
2023-06-25 00:16:25 +0600

    feat: Drawer sb docs

---
+++ docs/Drawer.stories.tsx
@@ -0,0 +1,195 @@
+import type { Meta, StoryObj, StoryContext } from "@storybook/react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import { useArgs } from "@storybook/addons";
+import { argProps, argChildren } from "./utils/formatArgs";
+import { Fragment } from "react";
+
+type Anchor = "top" | "left" | "bottom" | "right";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta<typeof Drawer> = {
+  title: "NAVIGATION/Drawer",
+  component: Drawer,
+  parameters: {
+    docs: {
+      source: { language: "tsx", format: true, type: "dynamic" },
+      description: {
+        component: "Another description, overriding the comments",
+      },
+      canvas: { sourceState: "shown" },
+    },
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    open: {
+      control: { type: "boolean" },
+    },
+    anchor: {
+      control: { type: "text" },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Drawer>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Template: Story = {
+  render: ({ onClick, toggleDrawer, anchor, open, ...args }: any) => {
+    const list = (anchor: Anchor) => (
+      <Box
+        sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+        role="presentation"
+        onClick={() => toggleDrawer(anchor, false)}
+      >
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    );
+    return (
+      <div>
+        {(["left", "right", "top", "bottom"] as const).map((anch) => (
+          <Fragment key={anch}>
+            <Button onClick={() => toggleDrawer(anch, true)}>{anch}</Button>
+            <Drawer
+              anchor={anchor}
+              open={open}
+              onClose={() => toggleDrawer(anchor, false)}
+            >
+              {list(anchor)}
+            </Drawer>
+          </Fragment>
+        ))}
+      </div>
+    );
+  },
+  args: {
+    open: false,
+  },
+  decorators: [
+    (Story) => {
+      const [args, updateArgs] = useArgs();
+      console.log("args", args);
+
+      const toggleDrawer = (anchor: Anchor, open: boolean) => {
+        console.log("first");
+        updateArgs({
+          open: open,
+          anchor: anchor,
+        });
+      };
+      return (
+        <>
+          <Story
+            args={
+              {
+                ...args,
+                toggleDrawer,
+              } as any
+            }
+          />
+        </>
+      );
+    },
+  ],
+  parameters: {
+    docs: {
+      source: {
+        transform: (code: string, storyContext: StoryContext): string => `
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+     
+const list = (anchor: Anchor) => (
+  <Box
+    sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+    role="presentation"
+    onClick={() => toggleDrawer(anchor, false)}
+  >
+    <List>
+      {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+        <ListItem key={text} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+    <Divider />
+    <List>
+      {["All mail", "Trash", "Spam"].map((text, index) => (
+        <ListItem key={text} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+  </Box>
+);
+
+<div>
+  {(["left", "right", "top", "bottom"] as const).map((anch) => (
+    <Fragment key={anch}>
+      <Button onClick={() => toggleDrawer(anch, true)}>{anch}</Button>
+      <Drawer
+        ${argProps(storyContext)}
+      >
+        {list(anchor)}
+      </Drawer>
+    </Fragment>
+  ))}
+</div>
+        `,
+      },
+    },
+  },
+};
