ed4c3649c42f07cac8ed8e0246387639afb0b42e
Ariful islam <arifulbgt4@gmail.com>
2023-06-27 13:57:36 +0600

    feat: Stepper sb docs

---
+++ docs/Stepper.stories.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import type { Meta, StoryObj, StoryContext } from "@storybook/react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepButton from "@mui/material/StepButton";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useArgs, useState } from "@storybook/addons";
+import { argProps, argChildren } from "./utils/formatArgs";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta<typeof Stepper> = {
+  title: "NAVIGATION/Stepper",
+  component: Stepper,
+  parameters: {
+    docs: {
+      source: { language: "tsx", format: true, type: "dynamic" },
+      description: {
+        component: "Another description, overriding the comments",
+      },
+      canvas: { sourceState: "shown" },
+    },
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    activeStep: {
+      control: { type: "number" },
+    },
+    nonLinear: {
+      control: { type: "boolean" },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Stepper>;
+
+const steps = [
+  "Select campaign settings",
+  "Create an ad group",
+  "Create an ad",
+];
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Template: Story = {
+  render: ({ onChange, handleStep, completed, activeStep, ...args }: any) => {
+    return (
+      <Stepper activeStep={activeStep} {...args}>
+        {steps.map((label, index) => (
+          <Step key={label} completed={completed[index]}>
+            <StepButton color="inherit" onClick={(e) => onChange(e, index)}>
+              {label}
+            </StepButton>
+          </Step>
+        ))}
+      </Stepper>
+    );
+  },
+  args: {
+    activeStep: 0,
+    nonLinear: true,
+  },
+  decorators: [
+    (Story) => {
+      const [args, updateArgs] = useArgs();
+      const [completed, setcompleted] = useState<{ [k: number]: boolean }>({
+        0: true,
+      });
+      return (
+        <>
+          <Story
+            args={
+              {
+                ...args,
+                completed,
+                onChange: (event: any, step: number) => {
+                  setcompleted((prevState) => ({
+                    ...prevState,
+                    [step]: true,
+                  }));
+                  updateArgs({
+                    activeStep: step,
+                  });
+                },
+              } as any
+            }
+          />
+        </>
+      );
+    },
+  ],
+  parameters: {
+    docs: {
+      source: {
+        transform: (code: string, storyContext: StoryContext): string => `
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepButton from "@mui/material/StepButton";
+     
+${code}
+        `,
+      },
+    },
+  },
+};
