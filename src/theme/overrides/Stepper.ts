64c26d715da9c874cb519b45d5c913e1bec003a7
Ariful islam <arifulbgt4@gmail.com>
2023-06-27 13:21:33 +0600

    feat: Stepper theme styles

---
+++ src/theme/overrides/Stepper.ts
@@ -0,0 +1,59 @@
+import { Theme, Components } from "@mui/material/styles";
+
+const MuiStepper: Components<Theme>["MuiStepper"] = {
+  styleOverrides: {
+    root: ({ theme, ownerState }) => ({}),
+  },
+};
+const MuiMobileStepper: Components<Theme>["MuiMobileStepper"] = {
+  styleOverrides: {
+    root: ({ theme, ownerState }) => ({}),
+  },
+};
+
+const MuiStep: Components<Theme>["MuiStep"] = {
+  styleOverrides: {
+    root: ({ theme, ownerState }) => ({}),
+  },
+};
+
+const MuiStepButton: Components<Theme>["MuiStepButton"] = {
+  styleOverrides: {
+    root: ({ theme, ownerState }) => ({}),
+  },
+};
+
+const MuiStepConnector: Components<Theme>["MuiStepConnector"] = {
+  styleOverrides: {
+    root: ({ theme, ownerState }) => ({}),
+  },
+};
+
+const MuiStepContent: Components<Theme>["MuiStepContent"] = {
+  styleOverrides: {
+    root: ({ theme, ownerState }) => ({}),
+  },
+};
+
+const MuiStepIcon: Components<Theme>["MuiStepIcon"] = {
+  styleOverrides: {
+    root: ({ theme, ownerState }) => ({}),
+  },
+};
+
+const MuiStepLabel: Components<Theme>["MuiStepLabel"] = {
+  styleOverrides: {
+    root: ({ theme, ownerState }) => ({}),
+  },
+};
+
+export default {
+  MuiStepper,
+  MuiMobileStepper,
+  MuiStep,
+  MuiStepButton,
+  MuiStepConnector,
+  MuiStepContent,
+  MuiStepIcon,
+  MuiStepLabel,
+};
