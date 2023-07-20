0aec8b02adeca627c96aaa5615265c117761b081
Mdjuealhossain <mdjueal920977@gmail.com>
2023-07-21 00:57:46 +0600

    fix: sb docs overview TextField.ts-theme override Textfield other components

--- src/theme/overrides/TextField.ts
+++ src/theme/overrides/TextField.ts
@@ -6,4 +6,48 @@ const MuiTextField: Components<Theme>["MuiTextField"] = {
   },
 };
 
-export default { MuiTextField };
+const MuiInput: Components<Theme>["MuiInput"] = {
+  styleOverrides: {
+    root: ({ theme, ownerState }) => ({}),
+  },
+};
+
+const MuiFilledInput: Components<Theme>["MuiFilledInput"] = {
+  styleOverrides: {
+    root: ({ theme, ownerState }) => ({}),
+  },
+};
+
+const MuiInputAdornment: Components<Theme>["MuiInputAdornment"] = {
+  styleOverrides: {
+    root: ({ theme, ownerState }) => ({}),
+  },
+};
+
+const MuiInputBase: Components<Theme>["MuiInputBase"] = {
+  styleOverrides: {
+    root: ({ theme, ownerState }) => ({}),
+  },
+};
+
+const InputLabel: Components<Theme>["MuiInputLabel"] = {
+  styleOverrides: {
+    root: ({ theme, ownerState }) => ({}),
+  },
+};
+
+const OutlinedInput: Components<Theme>["MuiOutlinedInput"] = {
+  styleOverrides: {
+    root: ({ theme, ownerState }) => ({}),
+  },
+};
+
+export default {
+  MuiTextField,
+  MuiInput,
+  MuiFilledInput,
+  MuiInputAdornment,
+  MuiInputBase,
+  InputLabel,
+  OutlinedInput,
+};
