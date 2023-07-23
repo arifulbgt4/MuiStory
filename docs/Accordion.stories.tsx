431ee9fba25dbf611814112765099b795e5a73e1
Ariful islam <arifulbgt4@gmail.com>
2023-07-23 19:00:16 +0600

    fix: sb components code write

--- docs/Accordion.stories.tsx
+++ docs/Accordion.stories.tsx
@@ -86,8 +86,8 @@ type Story = StoryObj<typeof Box>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Accordions: Story = {
-  args: {
-    children: (
+  render: (args) => {
+    return (
       <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
@@ -103,34 +103,38 @@ export const Accordions: Story = {
           </Typography>
         </AccordionDetails>
       </Accordion>
-    ),
+    );
   },
+  args: {},
   parameters: {
     docs: {
       source: {
         transform: (code: string, storyContext: StoryContext): string => `
-import Box from "@mui/material/Box";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-<Accordion>
-  <AccordionSummary
-    expandIcon={<ExpandMoreIcon />}
-    aria-controls="panel1a-content"
-    id="panel1a-header"
-  >
-    <Typography>Accordion 1</Typography>
-  </AccordionSummary>
-  <AccordionDetails>
-    <Typography>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-      malesuada lacus ex, sit amet blandit leo lobortis eget.
-    </Typography>
-  </AccordionDetails>
-</Accordion>
+export default function Basic() {
+  return (
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography>Accordion 1</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </Typography>
+      </AccordionDetails>
+    </Accordion>
+  );
+}
         `,
       },
     },
--- docs/AppBar.stories.tsx
+++ docs/AppBar.stories.tsx
@@ -94,23 +94,27 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 
-<AppBar ${argProps(storyContext)}>
-  <Toolbar>
-    <IconButton
-      size="large"
-      edge="start"
-      color="inherit"
-      aria-label="menu"
-      sx={{ mr: 2 }}
-    >
-      <MenuIcon />
-    </IconButton>
-    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-      News
-    </Typography>
-    <Button color="inherit">Login</Button>
-  </Toolbar>
-</AppBar>
+export default function Basic() {
+  return (
+    <AppBar ${argProps(storyContext)}>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          News
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+}        
         `,
       },
     },
