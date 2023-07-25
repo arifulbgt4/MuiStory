77de6236368e3b8a66a03658f33967b1d3c616ba
Mdjuealhossain <mdjueal920977@gmail.com>
2023-07-25 18:13:22 +0600

    fix: sb components source code override Tooltip.tsx

--- docs/Tooltip.stories.tsx
+++ docs/Tooltip.stories.tsx
@@ -14,15 +14,16 @@ const meta: Meta<typeof Tooltip> = {
       description: {
         component: overView({
           override: "Tooltip",
-          description: "Tooltips display informative text when users hover over, focus on, or tap an element.",
+          description:
+            "Tooltips display informative text when users hover over, focus on, or tap an element.",
           components: [
             {
               component: "Tooltip",
               selector: "MuiTooltip",
               props: "https://mui.com/material-ui/api/tooltip/#props",
-              css: "https://mui.com/material-ui/api/tooltip/#css"
-            }
-          ]
+              css: "https://mui.com/material-ui/api/tooltip/#css",
+            },
+          ],
         }),
       },
       canvas: { sourceState: "shown" },
@@ -50,13 +51,15 @@ type Story = StoryObj<typeof Tooltip>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Tooltips: Story = {
-  render: (args) => (
-    <Tooltip {...args}>
-      <IconButton>
-        <DeleteIcon />
-      </IconButton>
-    </Tooltip>
-  ),
+  render: (args) => {
+    return (
+      <Tooltip {...args}>
+        <IconButton>
+          <DeleteIcon />
+        </IconButton>
+      </Tooltip>
+    );
+  },
   args: {
     title: "Delete",
   },
@@ -68,13 +71,16 @@ import Tooltip from "@mui/material/Tooltip";
 import DeleteIcon from "@mui/icons-material/Delete";
 import IconButton from "@mui/material/IconButton";
 
-<Tooltip
-  ${argProps(storyContext)}
->
-  <IconButton>
-    <DeleteIcon />
-  </IconButton>
-</Tooltip>
+export default function Basic() {
+  return (
+    <Tooltip ${argProps(storyContext)} >
+      <IconButton>
+        <DeleteIcon />
+      </IconButton>
+    </Tooltip>
+  );
+}
+
         `,
       },
     },
