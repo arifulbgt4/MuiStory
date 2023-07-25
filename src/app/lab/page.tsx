d81a51693c9e8734d9e5b83c3bdf27498caac22c
Mdjuealhossain <mdjueal920977@gmail.com>
2023-07-25 18:03:10 +0600

    fix: sb components source code override Table.tsx (remove-import)

--- src/app/lab/page.tsx
+++ src/app/lab/page.tsx
@@ -2,10 +2,12 @@
 import Box from "@mui/material/Box";
 import Sidebar from "src/widgets/Sidebar";
 
-export default function LabPage() {
-  return (
-    <Box px={4}>
-      <Sidebar />
-    </Box>
-  );
-}
+import Tooltip from "@mui/material/Tooltip";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+
+<Tooltip title="Delete">
+  <IconButton>
+    <DeleteIcon />
+  </IconButton>
+</Tooltip>;
