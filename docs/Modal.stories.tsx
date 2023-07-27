89847429cc039d388277a5e58e688db3c9cb2bdc
mdjuealhossain <mdjueal920977>
2023-07-27 17:02:30 +0600

    fix: sb components source code override Modal.tsx

--- docs/Modal.stories.tsx
+++ docs/Modal.stories.tsx
@@ -6,7 +6,6 @@ import Button from "@mui/material/Button";
 import { useArgs } from "@storybook/addons";
 import { argProps, argChildren, overView } from "./utils/formatArgs";
 
-
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof Modal> = {
   title: "UTILS/Modal",
@@ -24,8 +23,8 @@ const meta: Meta<typeof Modal> = {
               component: "Modal",
               selector: "MuiModal",
               props: "https://mui.com/material-ui/api/modal/#props",
-              css: "https://mui.com/material-ui/api/modal/#css"
-            }
+              css: "https://mui.com/material-ui/api/modal/#css",
+            },
           ],
         }),
       },
@@ -81,7 +80,6 @@ export const Template: Story = {
             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
           </Typography>
         </Box>
-
       </Modal>
     );
   },
@@ -120,11 +118,53 @@ export const Template: Story = {
     docs: {
       source: {
         transform: (code: string, storyContext: StoryContext): string => `
+"use client";
+import React, { useState } from "react";
+
 import Modal from "@mui/material/Modal";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
-     
-${code}
+
+const style = {
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  left: "50%",
+  p: 4,
+  position: "absolute",
+  top: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+};
+
+export default function Basic() {
+  const [open, setOpen] = useState<boolean>(false);
+  return (
+    <Box>
+      <Button variant="contained" onClick={() => setOpen(true)}>Open Modal</Button>
+      <Modal open={open} onClose={() => setOpen(false)} ${argProps(
+        storyContext,
+        ["open"]
+      )}>
+        <Box sx={style}>
+          <Typography component="h2" id="modal-modal-title" variant="h6">
+            Text in a modal
+          </Typography>
+          <Typography
+            id="modal-modal-description"
+            sx={{
+              mt: 2,
+            }}
+          >
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </Box>
+  );
+}
+
         `,
       },
     },
