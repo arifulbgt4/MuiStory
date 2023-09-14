47dc71ca4535143f072bbf2d9e0736404a678e6a
md-cowsar-ahmed-257 <kawsarcse836@gmail.com>
2023-09-14 16:47:48 +0600

    fix:outlined card

--- docs/Card.stories.tsx
+++ docs/Card.stories.tsx
@@ -207,6 +207,56 @@ export const OutLined: Story = {
   args: {
     variant: "outlined",
   },
+  parameters: {
+    docs: {
+      source: {
+        transform: (code: string, storyContext: StoryContext): string => `
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+
+export default function Basic() {
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+    >
+      •
+    </Box>
+  );
+  return (
+    <Card ${argProps(storyContext)}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+        `,
+      },
+    },
+  },
 };
 export const RecipeReviewCard: Story = {
   render: (args) => {
