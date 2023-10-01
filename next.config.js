aa947e89660f7baf07df76cec5f251a5a78aa502
Ariful islam <arifulbgt4@gmail.com>
2023-10-02 01:58:12 +0600

    feat: next auth

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+};
 
 const withNextIntl = require("next-intl/plugin")(
   // This is the default (also the `src` folder is supported out of the box)
